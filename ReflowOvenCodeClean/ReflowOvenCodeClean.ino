1047c970a408ef9c63826a5aa6d0f4e3a38cfd79
Daria <dnesterovich2014@gmail.com>
2013-04-19 19:47:28 -0400

    I hope this works

--- ReflowOvenCodeClean/ReflowOvenCodeClean.ino
+++ ReflowOvenCodeClean/ReflowOvenCodeClean.ino
@@ -8,29 +8,28 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 int heaterPin = 13; // set heater pin to 13
 int tempPin = 1; // set temperature sensing pin to 1
-double setTemp = 23; // temp threshold in Celsius
+int setTemp = 23; // temp threshold in Celsius
 int lcd_key = 0; // 
 int adc_key_in = 0;
-int digit[] = {0, 0, 0, 0}; // the temperature to be set
 int cursorPos = 1;
 int check_key = 0;
 int shouldwesetthetemperature=0;
-
+int high = 300;
+int low = 20;
 
 
 
 void setup()
 {
+  
+  int startTemp = low; // start setting the Temp at 0.00
   lcd.begin(16,2);
   pinMode(heaterPin,OUTPUT); // heaterPin is set as an output
   lcd.setCursor(0,0);
   lcd.print("Set Temperature");
-  lcd.setCursor(0,1); 
-  lcd.print(digit[0]);
-  lcd.print(digit[1]);
-  lcd.print(digit[2]);
-  lcd.print("."); 
-  lcd.print(digit[3]);
+  lcd.setCursor(1,1); 
+  lcd.print(startTemp);
+  
 }
 
 
---
+++ ReflowOvenCodeClean/SetTempFunc.ino
@@ -0,0 +1,89 @@
+double setthetemperature()
+{
+  int settemperature = low; // the threshold value
+  int checkck = 0; 
+  
+  lcd_key = read_LCD_buttons();
+  while (checkck == 0) {
+      lcd_key = read_LCD_buttons();
+
+  
+  switch (lcd_key)   
+  {
+  // depending on which button was pushed, we perform an act:
+    case btnRIGHT:      // Instructions for what to do on RIGHT button press
+      {
+        if (cursorPos < 2) {
+             cursorPos = cursorPos + 1; }
+        else if (cursorPos == 2) {
+          cursorPos = 0;}
+          
+        lcd.setCursor(cursorPos,1);
+        break;
+      }
+    case btnLEFT:       // Instructions for what to do on LEFT button press
+      {
+      if (cursorPos > 0) {
+             cursorPos = cursorPos - 1; }
+      else if (cursorPos == 0) {
+        cursorPos = 2; }
+
+        lcd.setCursor(cursorPos,1);
+        break; 
+      }
+    case btnUP:
+      {
+        lcd.setCursor(cursorPos,1);
+        if (cursorPos = 0 && settemperature < 201) {
+          settemperature = settemperature + 100; }
+        else if (cursorPos = 1 && settemperature < 291) {
+          settemperature = settemperature + 10; }
+        else if (cursorPos = 2 && settemperature < 300) {
+          settemperature = settemperature + 1; }
+        
+        break;
+      }
+    case btnDOWN:
+      {
+        lcd.setCursor(cursorPos,1);
+        if (cursorPos = 0 && settemperature > 119) {
+          settemperature = settemperature - 100; }
+        else if (cursorPos = 1 && settemperature > 29) {
+          settemperature = settemperature - 10; }
+        else if (cursorPos = 2 && settemperature > 20) {
+          settemperature = settemperature - 1; }
+        break;
+      }
+      case btnSELECT:
+      {
+        int checker=0;
+
+        lcd.setCursor(0,0);
+        lcd.println("The Temperature ");       
+        lcd.setCursor(0,1);
+        lcd.print("is ");
+        lcd.print(settemperature);
+        setTemp = settemperature;
+        shouldwesetthetemperature=1;
+        delay(1000);
+        return setTemp=settemperature;
+      }
+      case btnNONE:
+      {
+      // Instructions for what to do if no button is pressed.
+       lcd.setCursor(cursorPos,1);
+       lcd.print(" ");
+       delay(100);
+      
+       lcd.setCursor(0,1);
+       lcd.print("                     ");
+       lcd.setCursor(0,1);
+       lcd.print(settemperature);
+       delay(100);
+
+       break;
+      }
+  }
+  delay(100);
+  }
+}
