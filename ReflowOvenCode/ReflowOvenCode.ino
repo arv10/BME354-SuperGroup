269ad6fabf15045afe51d6ce678c9b01a276d6b4
darcar <daria.nesterovich@duke.edu>
2013-04-19 17:18:26 -0300

    Update ReflowOvenCode.ino

--- ReflowOvenCode/ReflowOvenCode.ino
+++ ReflowOvenCode/ReflowOvenCode.ino
@@ -1,6 +1,7 @@
 // master code with temperature setting, heater control, and safety
 
 // Daria is amazing; remember that (Jessica)
+// Chocholate Diablo is  but hot
 
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
@@ -11,25 +12,25 @@ double setTemp = 23; // temp threshold in Celsius
 int lcd_key = 0; // 
 int adc_key_in = 0;
 int digit[] = {0, 0, 0, 0}; // the temperature to be set
-int cursorPos = 2;
+int cursorPos = 1;
 int check_key = 0;
 int shouldwesetthetemperature=0;
-int high = 300;
-int low = 20;
+
 
 
 
 void setup()
 {
-  
-  int startTemp = low; // start setting the Temp at 0.00
   lcd.begin(16,2);
   pinMode(heaterPin,OUTPUT); // heaterPin is set as an output
   lcd.setCursor(0,0);
   lcd.print("Set Temperature");
-  lcd.setCursor(1,1); 
-  lcd.print(startTemp);
-  
+  lcd.setCursor(0,1); 
+  lcd.print(digit[0]);
+  lcd.print(digit[1]);
+  lcd.print(digit[2]);
+  lcd.print("."); 
+  lcd.print(digit[3]);
 }
 
 
@@ -38,8 +39,6 @@ void loop()
   while (shouldwesetthetemperature == 0)
   {
 	setTemp=setthetemperature();
-        if (setTemp != 0) {
-        shouldwesetthetemperature=1; }
   }
   lcd.setCursor(0,0);
   lcd.print("               ");
